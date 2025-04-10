94d76983547c6852900aeaa4e1ad8fb1399fe71b
Ali Azimo <webdesign.aejl@gmail.com>
2025-04-10 17:06:42 +0200

    hoje

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    //Hero Silde
+        $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        smartSpeed: 1000,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        nav:true,
+        items: 1,
+        dots: false,
+        responsive:{
+            0:{
+               
+            },
+            700:{
+               
+            }
+        
+        }
+    })
+
+});
